e0212149a8287425c1a2edf947bb801552e8e9e6
documentable <bluufacebbye@gmail.com>
2025-03-27 21:00:11 -0700

    Update extkeybind.lua

--- ui/extkeybind.lua
+++ ui/extkeybind.lua
@@ -1,1 +1,35 @@
+-- hook.cc / ui/extkeybind.lua
 
+return function(hook_cc)
+    local Drawing = hook_cc.Drawing
+    local Input = hook_cc.Input
+    local Runtime = hook_cc.Runtime
+    local Dropdown = hook_cc.Dropdown
+
+    local ExtKeybind = {}
+
+    local keybindModes = {
+        "Hold",
+        "Toggle",
+        "Always On",
+        "Off on Hold"
+    }
+
+    function ExtKeybind:AttachTo(targetControl, position, currentMode, onSelect)
+        if not targetControl then return end
+
+        local dropdown = Dropdown:Create("Mode", position, 140, keybindModes, currentMode or "Hold", function(mode)
+            if typeof(targetControl.SetMode) == "function" then
+                targetControl:SetMode(mode)
+            end
+            if typeof(onSelect) == "function" then
+                pcall(onSelect, mode)
+            end
+        end)
+
+        targetControl.__extModeDropdown = dropdown
+        return dropdown
+    end
+
+    hook_cc.ExtKeybind = ExtKeybind
+end
