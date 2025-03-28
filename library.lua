c61753dab86be50cece526b8b7f76694b4f19328
documentable <bluufacebbye@gmail.com>
2025-03-27 19:02:49 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -1,18 +1,43 @@
 -- hook.cc / library.lua
+--[[
+                                                                
+                                                                
+  ,---,                              ,-.                        
+,--.' |                          ,--/ /|                        
+|  |  :       ,---.     ,---.  ,--. :/ |                        
+:  :  :      '   ,'\   '   ,'\ :  : ' /                         
+:  |  |,--. /   /   | /   /   ||  '  /        ,---.     ,---.   
+|  :  '   |.   ; ,. :.   ; ,. :'  |  :       /     \   /     \  
+|  |   /' :'   | |: :'   | |: :|  |   \     /    / '  /    / '  
+'  :  | | |'   | .; :'   | .; :'  : |. \   .    ' /  .    ' /   
+|  |  ' | :|   :    ||   :    ||  | ' \ \  '   ; :__ '   ; :__  
+|  :  :_:,' \   \  /  \   \  / '  : |--'___'   | '.'|'   | '.'| 
+|  | ,'      `----'    `----'  ;  |,'  /  .\   :    :|   :    : 
+`--''                          '--'    \  ; \   \  /  \   \  /  
+                                        `--" `----'    `----'   
+      stream zzzzombie
+]]
 
-local repo = "documentable/hook.cc" 
+
+local author = "documentable
+local repo = "hook.cc"
 local branch = "main"
 
 local function fetchModule(path)
-	local url = ("https://raw.githubusercontent.com/%s/%s/%s.lua"):format(repo, branch, path)
-	return loadstring(game:HttpGet(url, true), path)()
+	local url = ("https://raw.githubusercontent.com/%s/%s/%s/%s.lua"):format(author, repo, branch, path)
+	return loadstring(game:HttpGet(url, true), path .. ".lua")()
 end
 
 local hook_cc = {}
 
 fetchModule("core/init")(hook_cc)
-fetchModule("ui/main")(hook_cc)
-fetchModule("controls/main")(hook_cc)
+fetchModule("core/keycodes")(hook_cc)
+fetchModule("core/dependencies")(hook_cc)
+fetchModule("core/config")(hook_cc)
+
+fetchModule("ui/init")(hook_cc) 
+fetchModule("ui/extkeybind")(hook_cc)
+fetchModule("notify")(hook_cc) -- optional; only if you include this module
 
 _G.hook_cc = hook_cc
 return hook_cc
